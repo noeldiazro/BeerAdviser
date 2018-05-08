a711f3e0ed578731bd6fc8443a6411ea06c76f0c
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2018-05-08 10:45:16 +0200

    [2-4] Write logic

--- app/src/main/java/es/montanus/beeradviser/FindBeerActivity.java
+++ app/src/main/java/es/montanus/beeradviser/FindBeerActivity.java
@@ -3,8 +3,13 @@ package es.montanus.beeradviser;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class FindBeerActivity extends Activity {
+    private final BeerExpert beerExpert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,24 @@ public class FindBeerActivity extends Activity {
     }
 
     public void onClickFindBeer(View view) {
+        List<String> brands = beerExpert.getBrands(getBeerType());
+        setBrands(brands);
+    }
+
+    private String getBeerType() {
+        Spinner color = findViewById(R.id.color);
+        return String.valueOf(color.getSelectedItem());
+    }
+
+    private void setBrands(Iterable<String> brands) {
+        TextView brandsField = findViewById(R.id.brands);
+        brandsField.setText(format(brands));
+    }
+
+    private String format(Iterable<String> brands) {
+        final StringBuilder formattedBrands = new StringBuilder();
+        for (String brand: brands)
+            formattedBrands.append(String.format("%s%n", brand));
+        return formattedBrands.toString();
     }
 }
